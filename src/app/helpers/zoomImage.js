22130165681dbd069e975d617a597720c931e3a5
MaryutinVlad <louietrueman@gmail.com>
2023-07-14 13:00:44 +0300

    resizing edited

--- src/app/helpers/zoomImage.js
+++ src/app/helpers/zoomImage.js
@@ -3,38 +3,71 @@ export default function zoomImage(image) {
   const { width, height } = image.src
   const ratio = width >= height ? height / width : width / height
 
-  if (window.innerWidth >= window.innerHeight && width >= height) {
+  if (Math.abs(window.innerWidth - window.innerHeight) < 300) {
+    if (Math.abs(width - height) - 100 < 100) {
+      return {
+        ...image,
+        width: window.innerWidth * .6,
+        height: window.innerHeight * .6 * ratio
+      }
+    }
 
-    let shrinkFactor = 90
+    if (width > height) {
+      return {
+        ...image,
+        width: window.innerWidth * .75,
+        height: window.innerHeight * .75 * ratio
+      }
+    }
+  }
 
-    while (shrinkFactor) {
-      if (window.innerHeight * .9 > window.innerWidth * (shrinkFactor * .01) * ratio) {
-        shrinkFactor *= .01
-        break
+  if (window.innerWidth >= window.innerHeight) {
+
+    if (Math.abs(width - height) - 100 < 100) {
+      return {
+        ...image,
+        width: window.innerWidth * .4,
+        height: window.innerHeight * .4 * ratio
+      }
+    }
+
+    if (width >= height) {
+
+      
+      if (Math.abs(width - height) - 100 < 300) {
+        return {
+          ...image,
+          width: window.innerWidth * .5,
+          height: window.innerHeight * .5 * ratio
+        }
       }
 
-      shrinkFactor -= 1
+      return {
+        ...image,
+        width: window.innerWidth * .6,
+        height: window.innerWidth * .6 * ratio
+      }
     }
 
     return {
       ...image,
-      width: window.innerWidth * shrinkFactor,
-      height: window.innerWidth * shrinkFactor * ratio,
+      width: window.innerHeight * .8 * ratio,
+      height: window.innerHeight * .8
+    
     }
   }
 
   if (width >= height) {
-
     return {
       ...image,
-      width: window.innerWidth * .9,
-      height: window.innerWidth * .9 * ratio,
+      width: window.innerWidth,
+      height: window.innerWidth * ratio
     }
   }
 
   return {
     ...image,
-    width: window.innerHeight * .8 * ratio,
-    height: window.innerHeight * .8,
+    width: window.innerHeight * .7 * ratio,
+    height: window.innerHeight * .7
   }
 }
